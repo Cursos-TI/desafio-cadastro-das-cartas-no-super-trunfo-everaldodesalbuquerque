5730ec4fff2f2dcad655f364e1005dedc3d8b019
everaldodesalbuquerque <202407076742@alunos.unifbv.edu.br>
2025-03-28 01:32:05 +0000

    declaracao das variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,8 +2,24 @@
 
 int main(){
     printf("Desafio Super Trunfo!\n");
-    printf("Novo commit\n");
-    printf("Novo printf\n");
+
+    //Declaração e Inicialização das Variáveis
+    int Carta = 1; // Número da carta
+    int CartA = 2; // Número da cartA
+    char Estado = 'P'; // Armazena o Estado (exemplo: 'P' para Pernambuco) 
+    char EstadO = 'A'; // Armazena o EstadO (exemplo: 'A' para Alagoas)
+    char Codigo[4] = "P01"; // Código da Carta
+    char codigO[4] = "A01"; // Código da Carta
+    char Cidade[10] = "Recife"; // Nome da Cidade do Estado 'P'
+    char CidadE[10] = "Maceio"; // Nome da Cidade do Estado 'A'
+    int Populacao = 1490000; // Número de habitantes da Cidade do Recife, número inteiro 
+    int PopulacaO = 1375980; // Número de habitantes da Cidade de Maceió, número inteiro
+    float area_km2 = 218.84; // Área em Km², da Cidade de Recife, número decimal
+    float Area_km2 = 509.32; // Área em Km², da Cidade de maceió, número decimal
+    float PIB_Bilhoes_de_reais = 54.970; // PIB da cidade de Recife em bilhões, número decimal
+    float PIb_Bilhoes_de_reais = 76.070; // PIB da cidade de Maceió em bilhões, número decimal
+    int Pontosturisticos = 18; // Número de pontos turísticos da Cidade de Recife, número inteiro
+    int PontosTuristicos = 19; // Número de pontos turísticos da Cidade de Maceió, número inteiro
     
     
 
